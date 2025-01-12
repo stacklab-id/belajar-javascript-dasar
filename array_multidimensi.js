858956f33cb800f3b5f5746c0d1bb4dbeac64706
Stacklab <stacklabdev@gmail.com>
2025-01-13 01:11:07 +0700

    Update array_multidimensi.js

--- array_multidimensi.js
+++ array_multidimensi.js
@@ -12,38 +12,16 @@ const array2D = [
     ['Java', 'Springboot']
 ]
 
-// for (let i = 0; i < array2D.length; i++) {
-//     console.log(`No. ${i}`);
+for (let i = 0; i < array2D.length; i++) {
+    console.log(`No. ${i}`);
 
-//     for (let j = 0; j < array2D[i].length; j++) {
-//         if (j == 0) {
-//             console.log(`Bahasa: ${array2D[i][j]}`);
-//         } else if (j == 1) {
-//             console.log(`Framework: ${array2D[i][j]}`);   
-//         }
-
-//     }
-//     console.log('------------------------')
-// }
-
-let index = 0;
-for (let i of array2D) {
-    console.log(`No. ${index + 1}`)
-
-    let index_inner = 0;
-    for (let j of i) {
-        if (index_inner == 0) {
-            console.log(`Bahasa: ${j}`);
+    for (let j = 0; j < array2D[i].length; j++) {
+        if (j == 0) {
+            console.log(`Bahasa: ${array2D[i][j]}`);
         } else if (j == 1) {
-            console.log(`Framework: ${j}`);
-            
+            console.log(`Framework: ${array2D[i][j]}`);   
         }
 
-        index_inner++;
     }
-
-    console.log('-----------------------');
-    
-
-    index++;
+    console.log('------------------------')
 }
